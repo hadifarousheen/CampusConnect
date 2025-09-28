c250d8304c07038af779a1b0d36e7f65477f63eb
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2025-09-28 20:16:55 +0530

    change in socket.js for deployment

--- Frontend/src/utils/socket.js
+++ Frontend/src/utils/socket.js
@@ -1,14 +1,14 @@
 import { io } from "socket.io-client";
-export const createSocketConnection = () => {
-  return io(import.meta.env.VITE_BASE_URL
-);
-};
+// export const createSocketConnection = () => {
+//   return io(import.meta.env.VITE_BASE_URL
+// );
+// };
 
-// export const createSocketConnection=()=>{
-//     if(location.hostname==="localhost"){
-//         return io("https://localhost:3000")
-//     }
-//     else{
-//         return io("/",{path:"/api/socket.io"})
-//     }
-// }
+export const createSocketConnection=()=>{
+    if(location.hostname==="localhost"){
+        return io(import.meta.env.VITE_BASE_URL)
+    }
+    else{
+        return io("/",{path:"/api/socket.io"})
+    }
+}
