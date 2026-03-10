feae4efcc894c5f0e997b5eccc8e756868ff7608
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2026-03-10 16:21:09 +0530

    added .env to frontend

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
 // export const BASE_URL=location.hostname==="localhost"?"http://localhost:3000":"/api";
-export const BASE_URL=location.hostname==="localhost"?"http://localhost:3000":"process.env.REACT_APP_API_URL";
+export const BASE_URL=location.hostname==="localhost"?"http://localhost:3000":process.env.REACT_APP_API_URL;
 
