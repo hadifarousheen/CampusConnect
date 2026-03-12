480afeb4f081a3b9db76760a44b2486e44c501aa
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2026-03-12 21:10:31 +0530

    change in socket.js file

--- Frontend/src/utils/socket.js
+++ Frontend/src/utils/socket.js
@@ -1,11 +1,6 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
-export const createSocketConnection=()=>{
-    if(location.hostname==="localhost"){
-        return io(BASE_URL)
-    }
-    else{
-        return io("/",{path:"/socket.io"})
-    }
-}
+export const createSocketConnection = () => {
+  return io(BASE_URL);
+};
